a4da2918ecbab90b75d0ffb4d714741ccd880b81
Tolek Rudnicki <tolek@epsi.pl>
2022-02-27 21:55:23 +0100

    first commit

--- calendar.py
+++ calendar.py
@@ -4,9 +4,11 @@ todayfy = date.today()
 todayfy = str(todayfy)
 todayfy = todayfy.split("-")
 
-temp = ["","",""]
-temp[0] = todayfy[2]
-temp[1] = todayfy[1]
-temp[2] = todayfy[0]
+today = ["","",""]
+today[0] = todayfy[2]
+today[1] = todayfy[1]
+today[2] = todayfy[0]
 
-print(temp)
+
+
+print(today)
